917b8787f094213884e444acc95fcbc89d87a2a0
Tumelo <115115538+tumelosizani@users.noreply.github.com>
2025-04-23 00:32:34 +0200

    First commit

--- transaction-service/src/main/java/dev/dini/transactionservice/service/TransactionService.java
+++ transaction-service/src/main/java/dev/dini/transactionservice/service/TransactionService.java
@@ -1,10 +1,6 @@
 package dev.dini.transactionservice.service;
 
 import dev.dini.transactionservice.dto.PaymentCompletedEvent;
-import dev.dini.transactionservice.entity.Transaction;
-
-import java.util.List;
-import java.util.UUID;
 
 public interface TransactionService {
     void processCompletedEftPayment(PaymentCompletedEvent event);
